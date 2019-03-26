46dd4247c63301e47806bb8800755a30df3e86d6
ManojChandran <manoj.meparmbu@gmail.com>
2019-03-26 02:16:06 -0400

    updated comments

--- variables/main.go
+++ variables/main.go
@@ -6,19 +6,19 @@ import (
 )
 
 func main() {
-  // Explicit declaration
+  // static declaration
   var x int = 5
   var y int = 6
   var sum1 int = x + y
   fmt.Println(sum1)
 
-  // Implicit declaration
+  // dynamic declaration
   a := 5
   b := 6
   sum2 := a + b
   fmt.Println(sum2)
 
-  // In line declaration
+  // mixed declaration
   var d, e, f = 3, 4, "foo"
   fmt.Println(d)
   fmt.Println(e)
