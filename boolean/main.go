4de897a0742094c5094b39fa9699581d806bd99d
ManojChandran <manoj.meparmbu@gmail.com>
2019-03-26 14:25:47 -0400

    Boolean Example

---
+++ boolean/main.go
@@ -0,0 +1,16 @@
+// Boolean in go
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  var istrueornot bool
+  istrueornot = true
+  fmt.Println(istrueornot)
+  someBool := 5 < 3
+  fmt.Println(someBool)
+  isequal := 5 == 4
+  fmt.Println(isequal)
+}
