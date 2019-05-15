70e9d8f97d433e63e71856c86f1b1e919aab0f4b
ManojChandran <manoj.meparmbu@gmail.com>
2019-05-15 11:18:17 -0400

    package example added

--- 18_package_creation/main.go
+++ 18_package_creation/main.go
@@ -5,20 +5,20 @@ import (
   "os"
   "strconv"
 
-  "github.com/ManojChandran/gosample/17_package_creation/tempconv"
+  "./tempconv"
 )
 
 func main()  {
   for _,arg := range os.Args[1:]{
-    t, err := strconv.ParseFloat(arg, 64)
+    t,err := strconv.ParseFloat(arg, 64)
     if err != nil{
       fmt.Fprintf(os.Stderr, "cf : %v\n", err)
       os.Exit(1)
     }
-  }
-  f := tempconv.Fahrenhiet(t)
-  c := tempconv.Celsius(t)
+    f := tempconv.Fahrenhiet(t)
+    c := tempconv.Celsius(t)
 
-  fmt.Printf("%s = %s, %s = %s\n",
-            f, tempconv.FToC(c), c, tempconv.CToF(c))
+    fmt.Printf("%s = %s, %s = %s\n",
+            f, tempconv.FToC(f), c, tempconv.CToF(c))
+  }
 }
